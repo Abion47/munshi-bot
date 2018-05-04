ba4ae3d6d31d47fc19daded183b890716832c15f
Andrew Ackerman <andrew.ackie@gmail.com>
2018-05-04 10:19:18 -0600

    Added welcome message channel and guild member join event

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const welcomeChannels = {};
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -8,6 +10,29 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.content === '.ping') {
         msg.channel.send('pong');
+    }
+
+    if (msg.content.startsWith('.welcomeChannel')) {
+        let split = msg.split(' ', 2);
+        if (split.length != 2) { return }
+
+        let channels = msg.guild.channels;
+        for (let i = 0; i < channels; i++) {
+            if (channels[i].name === split[1]) {
+                welcomeChannels[msg.guild.name] = channels[i];
+                msg.channel.send(`Added ${split[1]} as a welcome channel for server ${msg.guild.name}.`);
+                break;
+            }
+        }
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    let guildName = member.guild.name;
+    let channel = welcomeChannels[guildName];
+    
+    if (channel) {
+        channel.send(`Oh hi. Didn't hear you come in. @${member.displayName}, right?  Please have a seat, and read the #rules.`);
     }
 });
 
